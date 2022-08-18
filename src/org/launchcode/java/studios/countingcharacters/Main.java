29fb53fe22fe7e0ee38b4276411dc05365017198
ahleem <72522827+AhleemAkinwande@users.noreply.github.com>
2022-08-18 15:01:25 -0400

    Studio 2: Character counter completed.

--- src/org/launchcode/java/studios/countingcharacters/Main.java
+++ src/org/launchcode/java/studios/countingcharacters/Main.java
@@ -1,9 +1,15 @@
 package org.launchcode.java.studios.countingcharacters;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
-        String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type a phrase to count");
+        String userPhrase = input.nextLine();
+        //String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
-        CharacterCounter.CountChar(phrase);
+        //CharacterCounter.CountChar(phrase);
+        CharacterCounter.CountChar(userPhrase.toLowerCase());
     }
 }
